52d9114090963a04c100f51c8ecc8fd6106790fe
liangdrew <liangdrew@gmail.com>
2017-04-27 11:33:05 -0400

    Implement /polls/get endpoint

--- main.go
+++ main.go
@@ -14,30 +14,65 @@ import (
 )
 
 type Request struct {
-	Question string    `json:"question"`
-	StoryID  string    `json:"storyId"`
-	PartID   string    `json:"partId"`
-	Choices  []Choices `json:"choices"`
+	Question string   `json:"question"`
+	StoryID  string   `json:"storyId"`
+	PartID   string   `json:"partId"`
+	Choices  []Choice `json:"choices"`
 }
 
-type Choices struct {
+type Response struct {
+	Question string    `json:"question"`
+	Created  time.Time `json:"created"`
+	Choices  []Choice  `json:"choices"`
+}
+
+type Choice struct {
 	Choice string `json:"choice"`
+	Votes  int    `json:"votes"`
+}
+
+type controller struct {
+	db *sql.DB
+}
+
+func newController(db *sql.DB) *controller {
+	return &controller{
+		db: db,
+	}
 }
 
 func main() {
-	http.HandleFunc("/health", healthCheck)
-	http.HandleFunc("/create", createPoll)
+	c := newController(getDB())
+	defer c.db.Close()
+	http.HandleFunc("/health", c.healthCheck)
+	http.HandleFunc("/polls/create", c.createPoll)
+	http.HandleFunc("/polls/get", c.getPoll)
 	err := http.ListenAndServe("localhost:8081", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func healthCheck(w http.ResponseWriter, r *http.Request) {
+func (c *controller) healthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
 }
 
-func createPoll(w http.ResponseWriter, r *http.Request) {
+func getDB() *sql.DB {
+	// Test connection to db
+	db, err := sql.Open("mysql", "root:root@/poll_service?parseTime=true")
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+	return db
+}
+
+func (c *controller) createPoll(w http.ResponseWriter, r *http.Request) {
 	// Read and decode request body
 	var req Request
 	decoder := json.NewDecoder(r.Body)
@@ -47,20 +82,10 @@ func createPoll(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	// Test connection to db
-	db, err := sql.Open("mysql", "root:root@/poll_service")
+	createPollStmt, err := c.db.Prepare("INSERT polls SET created=?, question=?, story_id=?, part_id=?")
 	if err != nil {
 		log.Fatalf("err: %s", err)
 	}
-	defer db.Close()
-
-	// Open doesn't open a connection. Validate DSN data:
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("err: %s", err)
-	}
-
-	createPollStmt, err := db.Prepare("INSERT polls SET created=?, question=?, story_id=?, part_id=?")
 	defer createPollStmt.Close()
 
 	datetime := time.Now().UTC()
@@ -69,7 +94,10 @@ func createPoll(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("err: %s", err)
 	}
 
-	createChoicesStmt, err := db.Prepare("INSERT INTO choices (choice, choice_index, votes, part_id) VALUES (?, ?, ?, ?)")
+	createChoicesStmt, err := c.db.Prepare("INSERT INTO choices (choice, choice_index, votes, part_id) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
 	defer createChoicesStmt.Close()
 	for i, c := range req.Choices {
 		_, err = createChoicesStmt.Exec(c.Choice, i+1, 0, req.PartID)
@@ -79,5 +107,41 @@ func createPoll(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, "OK")
+}
+
+func (c *controller) getPoll(w http.ResponseWriter, r *http.Request) {
+	partID := r.URL.Query().Get("partId")
+
+	var resp Response
+	getPollStmt, err := c.db.Prepare("SELECT question, created FROM polls WHERE part_id = ?")
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	err = getPollStmt.QueryRow(partID).Scan(&resp.Question, &resp.Created)
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	getChoicesStmt, err := c.db.Prepare("SELECT choice, votes FROM choices WHERE part_id = ? AND choice_index = ?")
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	for i := 1; i < 5; i++ {
+		var choice Choice
+		err = getChoicesStmt.QueryRow(partID, i).Scan(&choice.Choice, &choice.Votes)
+		if err != nil {
+			log.Fatalf("err: %s", err)
+		}
+		resp.Choices = append(resp.Choices, choice)
+	}
+
+	d, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	w.Write(d)
 
 }
